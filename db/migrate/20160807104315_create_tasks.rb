0416614328dbd5555138423123706eafeb065d34
bobpetar <bobpetar@gmail.com>
2016-08-07 23:10:40 +0000

    final fix

--- db/migrate/20160807104315_create_tasks.rb
+++ db/migrate/20160807104315_create_tasks.rb
@@ -4,9 +4,9 @@ class CreateTasks < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :picture
       t.references :user, foreign_key: true
-      t.integer :taken_by, default: false
+      t.integer :taken_by
       t.boolean :status, default: false
-      t.boolean :done
+      t.boolean :done, default: false
 
       t.timestamps
     end
